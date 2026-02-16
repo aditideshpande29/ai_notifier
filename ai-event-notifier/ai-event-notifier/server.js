0b1654f8bcfe1b1d23b27fcfbbfc00711fdd2962
aditideshpande29 <136156434+aditideshpande29@users.noreply.github.com>
2026-02-16 12:55:55 +0530

    Gemini model fix test

--- ai-event-notifier/ai-event-notifier/server.js
+++ ai-event-notifier/ai-event-notifier/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const axios = require("axios");
+console.log("Loaded API Key:", process.env.GEMINI_API_KEY);
 
 dotenv.config();
 
